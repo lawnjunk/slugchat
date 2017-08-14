e3e4214fc8a597245a8aa4f838184d93dbea4b42
Duncan Marsh <dwolfm@gmail.com>
2017-08-14 13:45:46 -0700

    added enforce https

---
+++ src/middleware/enforce-https.js
@@ -0,0 +1,11 @@
+export default (req, res, next) => {
+  console.log('forwareded-proto', req.headers['x-forwarded-proto'])
+  if(process.env.NODE_ENV !== 'production')
+    return next()
+  if(req.headers['x-forwarded-proto'] !== 'https'){
+    let secureURL = 'https://' + req.hostname + req.originalUrl)
+    console.log('secureURL', secureURL)
+    return res.redirect(secureURL)
+  }
+  next()
+}
